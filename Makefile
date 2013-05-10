a33739d549a4456b09c7115bd4af165bef621ffd
Andy Ibanez <leonnears@gmail.com>
2013-05-09 20:37:24 -0400

    First commit.

---
+++ Makefile
@@ -0,0 +1,6 @@
+ARCHS = armv7
+include theos/makefiles/common.mk
+BUNDLE_NAME = YOUR_BUNDLE_NAME
+YOUR_BUNDLE_NAME_FILES = YOURCLASS.mm
+YOUR_BUNDLE_NAME_INSTALL_PATH = /Library/Cydeswitch/plugins
+include $(THEOS_MAKE_PATH)/bundle.mk
---
+++ YOURCLASS.mm
@@ -0,0 +1,18 @@
+#import "Cydeswitch.h"
+
+@interface YOURCLASS : NSObject <Cydeswitch>
+-(void)ringerHasBeenMuted;
+-(void)ringerHasBeenUnmuted;
+@end
+
+@implementation YOURCLASS
+-(void)ringerHasBeenMuted
+{
+
+}
+
+-(void)ringerHasBeenUnmuted
+{
+
+}
+@end
